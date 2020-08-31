839dd4b9d200735e504fa89cdd5a77e8b0553eaf
Mohammad Amir Jamil <amirjamil900@gmail.com>
2020-08-31 11:41:39 +0530

    Correct answer

--- 2.py
+++ 2.py
@@ -1,26 +1,11 @@
-a=0
-b=1
-c=0
-z=[]
+a=1                #first term
+b=2
+t=1
 sum=0
-while(b<4000000):   
-   c=a+b
-   z.append(c) 
-   a=b
-   b=c
-   sum=sum+c
-s=0
-print(z)
-print(len(z))
-i=0
-even=0
-odd=0
-for i in range(len(z)):
-    if((i%2)==0):
-        print("hello",z[i])
-        even=even+z[i]
-    else:
-        odd=odd+z[i]
-        
-print(even)
-print(odd)
+while(b<=4000000):
+    if(b%2==0):
+        sum=sum+b           #sum of even valued numbers
+    t=a                 #swapping of numbers for the next term generation
+    a=b
+    b=t+b
+print(sum)                 #answer 
