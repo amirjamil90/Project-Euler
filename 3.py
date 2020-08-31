cc0cca0524b7997d0071dcfcbb90d842f1b22cb8
Mohammad Amir Jamil <amirjamil900@gmail.com>
2020-08-31 14:53:05 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 import math 
 maxPrime = -1
 n=600851475143
-while(n%2==0):
+while(n%2==0):                                     #sieve algo
   maxPrime=i
   n/=2
-for(i in range int(math.srt(n))+1,2):
+for(i in range int(math.srt(n))+1,2):                   #this checking is for Odd numbers 
   while(n%i==0):
     maxPrime=i
     n/=2
